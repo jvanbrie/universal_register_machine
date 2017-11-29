e6f26feefb68b801157de7faff2aeca3acecc95b
jvanbrie <jvanbrie@andrew.cmu.edu>
2017-11-29 14:37:13 -0500

    working register machine, compiler, and some of the necessary macros to create the universal register machine

---
+++ rm_macro_compiler.py
@@ -0,0 +1,145 @@
+
+import argparse
+verboseprint = lambda *a, **k: None
+
+def main(args):
+	global verboseprint
+	verboseprint = print if args.v else lambda *a, **k: None
+	
+	macro_files = ["copy.txt","zero.txt","pop.txt","dest_copy.txt","read.txt","mult_2.txt"]
+
+	command_file = open(args.input,'r')
+	output_file = open(args.output,'w+')
+	one_command = command_file.readline()
+
+	macros = {}
+	macro_lens = {}
+
+
+	# create macros dictionary 
+	# keys: macros
+	# values: [[variable list],macro string]
+	for macro in macro_files:
+		macro_file = open('macros' + '/' + macro,'r')
+		#title consists of name, and inputs
+		macro_title = macro_file.readline()
+		macro_title_list = (macro_title[:-1]).split()
+		macro_name = macro_title_list[0]
+		macros[macro_name] = [macro_title_list[1:],'']
+		macro_line = macro_file.readline()
+		macro_len = 0
+		while(macro_line != ''):
+			macro_len += 1
+			macros[macro_name][1] += macro_line
+			macro_line = macro_file.readline()
+		macro_lens[macro_name] = macro_len
+
+	registers = {}
+	commands = []
+
+	# count number of commands 
+	len_count = 0
+	while(one_command != ''):
+		len_count +=1
+		one_command = command_file.readline()
+
+	# get the length of each macro and put it in commands
+	commands = [0] * len_count
+	command_file = open(args.input,'r')
+	one_command = command_file.readline()
+	i = 0
+	ouput_index = 0
+	while(one_command != ''):
+		command_inputs = one_command[:-1].split()
+		commands[i] = ouput_index
+
+		# this try/except block skips non-macros
+		try:
+			macros[command_inputs[0]]
+		except:
+			ouput_index += 1
+			i += 1
+			one_command = command_file.readline()
+			continue
+
+		ouput_index += macro_lens[command_inputs[0]]
+		print(ouput_index)
+		i += 1
+		one_command = command_file.readline()
+
+
+	# write new no macro file
+	print(commands)
+	command_file = open(args.input,'r')
+	command_index = 0
+	one_command = command_file.readline()
+	while(one_command != ''):
+		if(one_command[-1] == '\n'):
+			one_command = one_command[:-1]
+		command_inputs = one_command.split()
+		print(command_inputs)
+		cur_macro = command_inputs[0]
+		try:
+			macros[cur_macro]
+		except:
+			for word_ind in range(len(command_inputs)):
+				replaced_word = command_inputs[word_ind]
+				try:
+					replaced_word = str(commands[int(replaced_word)])
+				except:
+					pass
+				command_inputs[word_ind] = replaced_word
+			output_file.write(' '.join(command_inputs) + '\n')
+			one_command = command_file.readline()
+			command_index += 1
+			continue
+		cur_macro_string = macros[cur_macro][1]
+		index_offset = commands[command_index]
+
+		print("new macro")
+
+		for var_ind in range(len(macros[cur_macro][0])):
+			# plus one due to the first value in command_inputs being the macro name rather than a variable
+			print(macros[cur_macro][0][var_ind])
+			print(command_inputs[var_ind + 1])
+			replaced_word = command_inputs[var_ind + 1]
+			try:
+				replaced_word = str(commands[int(replaced_word)])
+			except:
+				pass
+			print(replaced_word)
+			cur_macro_string = cur_macro_string.replace(macros[cur_macro][0][var_ind], replaced_word)
+		
+		print(cur_macro_string)
+		cur_macro_list = cur_macro_string.split(' ')
+
+
+		cur_macro_string = ' '.join(cur_macro_list)
+		output_file.write(cur_macro_string + '\n')
+		one_command = command_file.readline()
+
+		command_index += 1
+
+	output_file.close()
+
+		
+
+
+
+
+
+
+def parse_args():
+	parser = argparse.ArgumentParser(description='Splitting video transcripts')
+	parser.add_argument('-i', required = True, dest = 'input', action='store',
+	help='transcript file large')
+	parser.add_argument('-o', required = True, dest = 'output', action='store',
+	help='transcript file large')
+	parser.add_argument('-v', action='store_true', required = False,
+        help='enable verbose')
+
+	args = parser.parse_args()
+	print(main(args))
+
+parse_args()
+
---
+++ rm_simulator.py
@@ -0,0 +1,83 @@
+
+
+import argparse
+verboseprint = lambda *a, **k: None
+
+
+
+
+def main(args):
+	global verboseprint
+	verboseprint = print if args.v else lambda *a, **k: None
+	command_file = open(args.input,'r')
+	one_command = command_file.readline()
+
+	# macro registers
+	A = 0
+	B = 0
+	C = 0
+
+	registers = {}
+	commands = []
+
+	len_count = 0
+	while(one_command != ''):
+		len_count +=1
+		one_command = command_file.readline()
+
+	commands = [''] * len_count
+	command_file = open(args.input,'r')
+	one_command = command_file.readline()
+	i = 0
+	while(one_command != ''):
+		commands[i] = one_command[:-1]
+		i += 1
+		one_command = command_file.readline()
+
+	def print_state(line):
+		print ("State after line %d:" % line)
+		print (registers)
+
+	line = 0
+
+	while(True):
+		command = commands[line]
+		if command == 'hal':
+			break
+		command_inputs = command.split()
+		verboseprint(command_inputs)
+		if(len(command_inputs) == 3):
+			try:
+				registers[command_inputs[1]] += 1
+			except:
+				registers[command_inputs[1]] = 1
+			if(args.v):
+				print_state(line)
+			line = (int(command_inputs[2]))
+		else:
+			try:
+				registers[command_inputs[1]]
+			except:
+				registers[command_inputs[1]] = 0
+			if registers[command_inputs[1]] == 0:
+				line = (int(command_inputs[3]))
+			else:
+				registers[command_inputs[1]] -= 1
+				line = (int(command_inputs[2]))
+	print("Final state:")
+	print_state(0)
+
+
+
+def parse_args():
+	parser = argparse.ArgumentParser(description='Splitting video transcripts')
+	parser.add_argument('-i', required = True, dest = 'input', action='store',
+	help='transcript file large')
+	parser.add_argument('-v', action='store_true', required = False,
+        help='enable verbose')
+
+	args = parser.parse_args()
+	print(main(args))
+
+parse_args()
+
